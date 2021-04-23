e3b11dbd845a81c140ee0fa21bfcbdcbf38c4f9c
mohdarshad1 <mdarshad181@gmail.com>
2021-04-23 19:09:28 +0530

    [Mohd Arshad] : UC13 Daily Wage with Total Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,7 +6,7 @@ public class EmployeeWage implements ICalculateWage {
 	private int noofcompanies = 0;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation\n");
+		System.out.println("Welcome to employee wage computation\n");
 		ICalculateWage employeewage = new EmployeeWage();
 		employeewage.CompanyInfo();
 
@@ -17,23 +17,23 @@ public class EmployeeWage implements ICalculateWage {
 		Scanner scanner = new Scanner(System.in);
 		int choice = 0;
 		do {
-			System.out.println("Add Company Details");
-			System.out.println("Enter your Company Name");
+			System.out.println("plase add company details");
+			System.out.println("Enter your Company name");
 			String companyName = scanner.nextLine();
 			System.out.println("Enter wage per hour");
 			int wagePerHour = scanner.nextInt();
-			System.out.println("Enter no of working days per month");
+			System.out.println("enter no of working days per month");
 			int noofWorkingDays = scanner.nextInt();
 			System.out.println("Enter the no of max hours per month");
 			int noofMaxHours = scanner.nextInt();
-			System.out.println("Do ypu want to add more?\n" + "1. Yes\n" + "2. No\n" + "Enter your Choice");
+			System.out.println("do ypu want to add more?\n" + "1. yes\n" + "2. no\n" + "Enter your choice");
 			choice = scanner.nextInt();
 			addCompany(companyName, wagePerHour, noofWorkingDays, noofMaxHours);
 			scanner.nextLine();
 			noofcompanies++;
 		} while (choice == 1 && noofcompanies < 5);
 		if (noofcompanies == 5)
-			System.out.println("Max number of Companies Reached");
+			System.out.println("max number of companies reached");
 		showWage();
 		scanner.close();
 	}
@@ -52,12 +52,14 @@ public class EmployeeWage implements ICalculateWage {
 		final int fullTime = 1;
 		final int partTime = 2;
 		int EmpWage = 0;
+		float EmpDaiyWage=0;
 		int noofdays = 0;
 		int noofhours = 0;
 		int value;
+		int noofdaysworked=0;
 
 		while (noofdays < calcwage.getNoofWorkingDays() && noofhours < calcwage.getNoofMaxHours()) {
-
+			noofdaysworked++;
 			value = (int) ((Math.random() * 10) % 3);
 
 			switch (value) {
@@ -81,8 +83,10 @@ public class EmployeeWage implements ICalculateWage {
 			int wage = calcwage.getWagePerHour() * fullDayHour;
 			EmpWage = EmpWage + wage;
 		}
-
+		
+		EmpDaiyWage=(float)EmpWage/noofdaysworked;
 		calcwage.setTotalWage(EmpWage);
+		calcwage.setDailyWage(EmpDaiyWage);
 	}
 
 	public void showWage() {
@@ -99,6 +103,7 @@ class CompanyWage {
 	private int noofWorkingDays;
 	private int noofMaxHours;
 	private String companyName;
+	private float dailyWage;
 	private int totalWage;
 
 	public CompanyWage(int wagePerHour, int noofWorkingDays, int noofMaxHours, String companyName) {
@@ -140,6 +145,15 @@ class CompanyWage {
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
+	
+	public float getDailyWage() {
+		return dailyWage;
+	}
+
+	public void setDailyWage(float dailyWage) {
+		this.dailyWage = dailyWage;
+	}
+
 
 	public int getTotalWage() {
 		return totalWage;
